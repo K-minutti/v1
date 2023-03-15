2b1b5f2b89869dcbd220675aa78766626a68753a
Kevin Minutti <minutti70@gmail.com>
2023-03-15 16:20:42 -0400

    Update links and fix images optimization issue

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {protocol: 'https',
+      hostname: 'githubusercontent.com',
+      port: '',
+      pathname: '/K-minutti/**'
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
