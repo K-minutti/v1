0700fb068f313d16579d096c875bd1391a660bdb
Kevin Minutti <minutti70@gmail.com>
2023-03-21 21:48:35 -0400

    Upgrade to use mdx and add code block styling

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -1,12 +1,22 @@
 import { getAllPostIds, getPostData } from '../../../lib/posts';
+import { GetStaticProps } from 'next';
 import Date from '../../../components/date';
 import styles from '@/styles/post.module.css'
 import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
 
-export async function getStaticProps({ params }: any) {
+import { MDXRemote } from 'next-mdx-remote';
+const components = {
+  Head,
+  Image,
+  Link,
+};
+
+// export async function getStaticProps({ params }: any)
+export const getStaticProps: GetStaticProps = async ({ params }: any) => {
     // Add the "await" keyword like this:
     const postData = await getPostData(params.id);
-  
     return {
       props: {
         postData,
@@ -31,15 +41,17 @@ export default function Post({postData}: any) {
         </Head>
         <br />
         <article className={styles.article}>
-        <div className={styles.header}>
-        <h1 >{postData.title}</h1>
-        </div>
-        <div>
-          <Date dateString={postData.date} />
-        </div>
-        <section className={styles.body}>
-        <div className={styles.content} dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-        </section>
+          <div className={styles.header}>
+          <h1 >{postData.title}</h1>
+          </div>
+          <div>
+            <Date dateString={postData.date} />
+          </div>
+          <section className={styles.body}>
+          <MDXRemote {...postData.source} components={components} />
+          {/* <div className={styles.content} dangerouslySetInnerHTML={{ __html: postData.contentHtml }} /> */}
+        
+          </section>
       </article>
       </div>
     );
